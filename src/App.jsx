443371257e67a8d6dc5bfb91612392cf1290b7fd
Faisal <pros3sa@gmail.com>
2024-12-06 20:14:56 +0000

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Homepage from './pages/Homepage'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Homepage />
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import logo from '../assets/logo.png'
+import logo from '../assets/Logo.png'
+import { NavLink } from 'react-router-dom'
+import { IoIosArrowRoundForward } from "react-icons/io";
+
 
 const Header = () => {
   return (
-    <div className='footer'>
+    <div className='header'>
+        <img src={logo} alt="logo" />
+        <nav>
+            <ul>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/features'>Features</a></li>
+                <li><a href='/community'>Community</a></li>
+                <li><a href='/blog'>Blog</a></li>
+                <li><a href='/pricing'>Pricing</a></li>
+                <li className='active'><a href='/register'>Register Now<IoIosArrowRoundForward  style={{color:'white'}}/></a></li>
+            </ul>
+        </nav>
       
     </div>
   )
