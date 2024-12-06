244116eaeeef3887247ec4166b83314690c94991
Faisal <pros3sa@gmail.com>
2024-12-06 20:58:40 +0000

    finished clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import rings from '../assets/rings.png';
+import net from '../assets/net.png';
+import ipsumlogo from '../assets/logoipsum.png';
+import oj from '../assets/oj.png';
+import spaceship from '../assets/spaceship.png';
+import yingyang from '../assets/yingyang.png';
+
 
 const Clients = () => {
   return (
     <div className='clients'>
-        <h2>Our Clients</h2>
+        <h1>Our Clients</h1>
         <p>We have been working wit some fortune 500+ clients</p>
+
+        <div className="images">
+            <img src={yingyang} alt="" />
+            <img src={spaceship} alt="" />
+            <img src={oj} alt="" />
+            <img src={ipsumlogo} alt="" />
+            <img src={net} alt="" />
+            <img src={rings} alt="" />
+            <img src={oj} alt="" />
+            
+        </div>
       
     </div>
   )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Body from '../components/Body'
+import Clients from '../components/Clients'
 
 const Homepage = () => {
   return (
     <div className='home'>
       <Header />
       <Body />
+      <Clients />
     </div>
   )
 }
