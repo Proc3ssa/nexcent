72dddbb2dfc984157f2962464b8d6d057f0036ac
Faisal <pros3sa@gmail.com>
2024-12-07 06:44:42 +0000

    finshed experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,9 +5,13 @@ const Experience = () => {
   return (
     <div className='experience'>
         <div className="img">
-     <img src={illustration} alt="" />
+           <img src={illustration} alt="" />
         </div>
-        <div className="texts"></div>
+        <div className="texts">
+            <h1>The unseen of spending three years at Pixelgrade</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus tempore hic vero non at dolorum qui doloribus fugiat, placeat odio!</p>
+            <button className='active'>Learn more</button>
+        </div>
       
     </div>
   )
