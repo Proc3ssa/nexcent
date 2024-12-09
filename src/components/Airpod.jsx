aba7dfb490cec58482ee1b57bd639859a25fa5a5
Faisal <pros3sa@gmail.com>
2024-12-09 10:15:20 +0000

    Airpod

--- src/components/Airpod.jsx
+++ src/components/Airpod.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import airpod from '../assets/airpod.png'
+import image from '../assets/airpod.png'
+import rings from '../assets/rings.png';
+import net from '../assets/net.png';
+import ipsumlogo from '../assets/logoipsum.png';
+import oj from '../assets/oj.png';
+import spaceship from '../assets/spaceship.png';
+import yingyang from '../assets/yingyang.png';
+import { Link } from 'react-router-dom';
+import { IoIosArrowRoundForward } from 'react-icons/io';
 
 const Airpod = () => {
   return (
@@ -8,9 +16,21 @@ const Airpod = () => {
            <img src={image} alt="" />
         </div>
         <div className="texts">
-            <h1>{header}</h1>
-            <p>{text}</p>
-            <button className='active'>Learn more</button>
+            
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus illo natus quam sunt dignissimos repellendus beatae nesciunt modi magni dicta laboriosam sit odit, quod aliquam possimus ipsam? Aspernatur, labore!</p>
+            <h3>Faisal Halid Dene</h3>
+            <p className='up'>Long Life Holdings</p>
+            <div className="images">
+            <img src={yingyang} alt="" />
+            <img src={spaceship} alt="" />
+            <img src={oj} alt="" />
+            <img src={ipsumlogo} alt="" />
+            <img src={net} alt="" />
+            
+            
+            <a href='/clients'><h3 style={{color: '#237D31'}}>Meet all clients <IoIosArrowRoundForward /></h3></a>
+            
+        </div>
         </div>
       
     </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,6 +6,7 @@ import Community from '../components/Community'
 import Experience from '../components/Experience'
 import Reinvent from '../components/Reinvent'
 import illustration from '../assets/rafiki.png'
+import Airpod from '../components/Airpod'
 import pana from '../assets/pana.png'
 
 const Homepage = () => {
@@ -18,6 +19,8 @@ const Homepage = () => {
       <Experience image={illustration} header='The unseen of spending three years at Pixelgrade' text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus tempore hic vero non at dolorum qui doloribus fugiat, placeat odio!'/> 
       <Reinvent />
       <Experience image={pana} header='How to design your footer like this' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse magni error rem eveniet aut atque perspiciatis facilis architecto animi veniam sint nulla provident, asperiores quibusdam ab officiis blanditiis maiores inventore!'/>
+
+      <Airpod />
      
       
     </div>
