2abb1ec024e14455fcc67a58946729608a495d4a
Faisal <pros3sa@gmail.com>
2024-12-11 07:20:20 +0000

    finished with register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Homepage from './pages/Homepage'
+import Register from './pages/Register'
+import { Route, 
+  createBrowserRouter, 
+  RouterProvider,
+  createRoutesFromElements } from 'react-router-dom'
+import Mainoutlet from './outlets/Mainoutlet'
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Mainoutlet />}>
+        <Route index element={<Homepage />} />
+        <Route path='/register' element={<Register />}/>
+      </Route>
+    )
+  )
 const App = () => {
   return (
-    <>
-      <Homepage />
-    </>
+   <RouterProvider router={router} />
   )
 }
 
