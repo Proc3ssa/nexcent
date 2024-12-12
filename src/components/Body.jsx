bebeb3acfb7c459d8475081e58b8885858eee7a8
Faisal <pros3sa@gmail.com>
2024-12-12 08:09:05 +0000

    done

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import illustration from '../assets/Illustration.png'
+import { useNavigate } from 'react-router-dom'
 
 const Body = () => {
+  const navigate = useNavigate();
+
+  const register = () =>{
+    navigate('/register')
+  }
   return (
     <div className='body'>
         <div className="left">
             <h1>Lessons and insights 
             <span> from 8 years</span></h1>
             <p>Where to grow your business as a photographer on social media</p>
-            <button className='register'>
+            <button className='register' onClick={register}>
                 Register
             </button>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import logo from '../assets/Logo.png'
 import { NavLink } from 'react-router-dom'
 import { IoIosArrowRoundForward } from "react-icons/io";
-
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdCloseCircle } from 'react-icons/io';
 
 const Header = () => {
+  
+  
   return (
-    <div className='header'>
-        <img src={logo} alt="logo" />
-        <nav>
-            <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/features'>Features</a></li>
-                <li><a href='/community'>Community</a></li>
-                <li><a href='/blog'>Blog</a></li>
-                <li><a href='/pricing'>Pricing</a></li>
-                <li className='active'><a href='/register'>Register Now<IoIosArrowRoundForward  style={{color:'white'}}/></a></li>
-            </ul>
-        </nav>
-      
-    </div>
+    <nav>
+    
+    <input type="checkbox" id="check" />
+   
+    <label for="check" class="checkbtn">
+      <GiHamburgerMenu  style={{color:'black'}}/>
+    </label>
+    
+    <label class="logo"><img src={logo} alt="" /></label>
+    
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Features</a></li>
+      <li><a href="#">Community</a></li>
+      <li><a href="#">Blog</a></li>
+      <li className='active'><a href="#" >Register <IoIosArrowRoundForward /></a></li>
+    </ul>
+  </nav>
   )
 }
 
